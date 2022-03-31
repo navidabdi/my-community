9b76c876ba63d301d25f2ca9f2f4038fe0b7311c
navidabdi <artsanaat@gmail.com>
2022-03-31 10:59:17 +0430

    fetch first data => useFeed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { simplifyPaginatedResult } from '@tribeplatform/react-sdk/utils';
+import { Post } from '@tribeplatform/gql-client/types';
+import { useFeed } from '@tribeplatform/react-sdk/hooks';
+
 function App() {
+  const { data } = useFeed({
+    fields: {
+      createdBy: {
+        member: 'basic',
+      },
+    },
+    variables: {
+      limit: 10,
+    },
+  });
+  const { nodes: posts } = simplifyPaginatedResult<Post>(data);
+
   return (
-    <div className="app">
-      <h1>Hello Word</h1>
+    <div className="lg:w-3/4 m-auto flex flex-col">
+      {posts.map((post, i) => (
+        <div className="flex gap-2 bg-hacker-body p-2" key={post?.id}>
+          <div className="flex flex-col justify-center">{i + 1}.</div>
+          <div className="flex flex-col flex-grow">
+            <div>{post.title}</div>
+            <div className="flex gap-2 text-xs text-gray-500">
+              <div>By {post.createdBy?.member?.name}</div>|
+              <div>{post.reactionsCount} upvotes</div>|
+              <div>{post.repliesCount} comments</div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { Provider as TribeProvider } from '@tribeplatform/react-sdk';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TribeProvider
+      config={{
+        baseUrl: 'https://app.tribe.so/graphql',
+        networkDomain: 'nabiabdi.tribeplatform.com',
+        accessToken: localStorage.getItem('accessToken'),
+      }}
+    >
+      <App />
+    </TribeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
